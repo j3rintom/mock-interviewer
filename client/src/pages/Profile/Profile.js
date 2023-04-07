5e82add8ac34d2f570baad818e1e10df1263596b
Jerin Tom <jerintom@Jerins-MacBook-Pro.local>
2023-04-07 20:07:21 +0530

    started profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+
 import Navbar2 from '../../components/Navbar2/Navbar2'
 import "./Profile.css"
+import { UserAuth} from '../../context/AuthContext';
+import {  useNavigate } from 'react-router-dom'
 const Profile = () => {
+  const navigate = useNavigate()
+    const {logout,user}  = UserAuth()
+  const handleLogout = async ()=>{
+      try {
+        await logout();
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  useEffect(()=>{
+    if(user == null){
+        navigate('/')
+    }
+},[user])
   return (
-    <div>
-        <Navbar2 />
-      Profile
-    </div>
+    <>
+      <Navbar2 />
+      <div className="profile-container">
+        <div className='logout-container'>
+        <h2>Profile</h2>
+          <button className="button-24"  onClick={handleLogout}>Logout</button> 
+        </div>
+        <div className='user-info'>
+        <img alt={user.displayName} src={user.photoURL} />
+          <h1>{user.displayName}</h1>
+          <h2>{user.email}</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
